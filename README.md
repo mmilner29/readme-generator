0e32c481091b8345470c1f0adc8abbcc102b7bb1
mmilner29 <madisonm4514@gmail.com>
2022-03-26 16:16:36 -0700

    add license section

--- README.md
+++ README.md
@@ -2,33 +2,37 @@
   # Professional README Generator
 
   ## Description
-  I wanted to allow users to generate a professional README for future projects in a quick and easy way.
+  I was motivated to design a README generator to provide users an easy way to create a professional readme for their projects.
 
   ## Table of Contents
-  -[Installation](#installation)
-  -[Usage](#usage)
-  -[Contributions](#contributions)
-  -[License](#license)
-  -[Tests](#tests)
+  -[Installation](#installation)  
+  -[Usage](#usage)  
+  -[Contributions](#contributions)  
+  -[License](#license)  
+  -[Tests](#tests)  
   -[Questions](#questions)
 
   ## Installation
-    download code
+  npm init then npm install inquirer
 
   ## Usage
-  Answer prompt questions to generate a readme for any project
+  Run node index at the root of your project and answer the questions.
 
   ## Contributions
-  Fork & create PR
+  They can fork the project and create a PR
 
+  
   ## License
-  MIT
+  
+  This application is covered by the MIT license.
+  
 
   ## Tests
-  IDK
+  tests
 
   ## Questions
-  Github: https://github.com/mmilner29
+  Please contact me using the following:  
+  Github: https://github.com/mmilner29  
   Email: madisonm4514@gmail.com
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
+  } else if (license === 'GNU GPLv3') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+  } else if (license === 'GNU AGPLv3') {
+    return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '(https://opensource.org/licenses/MIT)';
+  } else if (license === 'GNU GPLv3') {
+    return '(https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'GNU AGPLv3') {
+    return '(https://www.gnu.org/licenses/agpl-3.0)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+  ## License
+  ${renderLicenseBadge(license) + renderLicenseLink(license)}
+  This application is covered by the ${license} license.
+  `
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -19,11 +46,11 @@ function generateMarkdown(data) {
   ${data.description}
 
   ## Table of Contents
-  -[Installation](#installation)
-  -[Usage](#usage)
-  -[Contributions](#contributions)
-  -[License](#license)
-  -[Tests](#tests)
+  -[Installation](#installation)  
+  -[Usage](#usage)  
+  -[Contributions](#contributions)  
+  -[License](#license)  
+  -[Tests](#tests)  
   -[Questions](#questions)
 
   ## Installation
@@ -35,14 +62,14 @@ function generateMarkdown(data) {
   ## Contributions
   ${data.contribute}
 
-  ## License
-  ${data.license}
+  ${renderLicenseSection(data.license)}
 
   ## Tests
   ${data.test}
 
   ## Questions
-  Github: https://github.com/${data.github}
+  Please contact me using the following:  
+  Github: https://github.com/${data.github}  
   Email: ${data.email}
 
 
